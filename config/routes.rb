61e9981e98e4892a1b5d7e655bbb08cd2045da76
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-04-07 22:51:32 +0100

    feat: create recipe controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get 'recipes/show'
+      delete 'recipes/destroy'
+    end
+  end
   root 'homepage#index'
+  get '/*path' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
