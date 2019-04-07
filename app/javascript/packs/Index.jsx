a37f362d0d5797c4397c3442c3d0444658363eba
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-04-07 16:22:43 +0100

    feat: configure react frontend
    - add routing with react router
    - add home component
    - add default recipe image

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -2,25 +2,11 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from "react";
+import { render } from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "../components/App";
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+document.addEventListener("DOMContentLoaded", () => {
+  render(<App />, document.body.appendChild(document.createElement("div")));
+});
