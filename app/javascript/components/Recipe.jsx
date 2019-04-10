9858811f5be7edb50521e88ee542427169ae147d
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-04-10 10:55:56 +0100

    feat: view recipe

---
+++ app/javascript/components/Recipe.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Recipe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { recipe: { ingredients: "" } };
+  }
+
+  componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      }
+    } = this.props;
+
+    const url = `/api/v1/show/${id}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(response => this.setState({ recipe: response }));
+  }
+
+  render() {
+    const { recipe } = this.state;
+    let ingredientList = "No ingredients available";
+
+    if (recipe.ingredients.length > 0) {
+      ingredientList = recipe.ingredients
+        .split(",")
+        .map((ingredient, index) => (
+          <li key={index} className="list-group-item">
+            {ingredient}
+          </li>
+        ));
+    }
+
+    return (
+      <div className="">
+        <div className="hero position-relative d-flex align-items-center justify-content-center">
+          <img
+            src={recipe.image}
+            alt={`${recipe.name} image`}
+            className="img-fluid position-absolute"
+          />
+          <div className="overlay bg-dark position-absolute" />
+          <h1 className="display-4 position-relative text-white">
+            {recipe.name}
+          </h1>
+        </div>
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-sm-12 col-lg-3">
+              <ul className="list-group">
+                <h5 className="mb-2">Ingredients</h5>
+                {ingredientList}
+              </ul>
+            </div>
+            <div className="col-sm-12 col-lg-7">
+              <h5 className="mb-2">Preparation Instriction</h5>
+              <p>{recipe.instruction}</p>
+            </div>
+            <div className="col-sm-12 col-lg-2">
+              <button type="button" className="btn btn-danger">
+                Delete Recipe
+              </button>
+            </div>
+          </div>
+          <Link to="/recipes" className="btn btn-link">
+            Back
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Recipe;
