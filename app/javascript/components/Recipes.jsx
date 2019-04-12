19dd8becf4418b0a54dd3295da562aa823e9ab49
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-04-12 22:00:13 +0100

    chore: add error handling to

--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -12,8 +12,14 @@ class Recipes extends React.Component {
   componentDidMount() {
     const url = "/api/v1/recipes/index";
     fetch(url)
-      .then(response => response.json())
-      .then(response => this.setState({ recipes: response }));
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.setState({ recipes: response }))
+      .catch(() => this.props.history.push("/"));
   }
 
   render() {
